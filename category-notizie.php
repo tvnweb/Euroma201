b4b825bd8b532322f9767ca2350206a7a880fa8f
tvnweb <alessandro.bassi@tvnmediagroup.it>
2017-05-23 13:01:14 +0200

    News e pagina single.php

--- category-notizie.php
+++ category-notizie.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 
-<div id="griglia-promo">
+
 	<div class="container">
 		<div class="row">
 			<?php
-			$news_args = array('post_type' => 'news','cat' => '414', 'posts_per_page' => 100, 'orderby' => 'date');
+			$news_args = array('post_type' => 'news','cat' => '414', 'posts_per_page' => 7, 'orderby' => 'date');
 			$news = new WP_Query($news_args);
 
 			while($news->have_posts()) : $news->the_post();
@@ -12,13 +12,23 @@
 			?>
 			<div class="small-12">
 
+
+				<div class="newsbox">
+				<div class="newsthumb">
 				<a href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail('eventi'); ?>
-				<span><?php echo the_date(); ?></span>
-				<b><?php echo the_title(); ?></b>
+				<?php the_post_thumbnail('prodotti_dettaglio'); ?>
 				</a>
+				</div>
+				<div class="newsinfo">
+				<span><?php the_date(); ?></span>
+				<b><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></b>
+				<p><?php the_excerpt(); ?></p>
+				</div>
+				</div>
+
 
 			</div>
+			<div class="small-12"><hr/></div>
 
 			<?php
 
@@ -26,6 +36,6 @@
 
     </div>
 	</div>
-</div>
+
 
 <?php get_footer(); ?>
